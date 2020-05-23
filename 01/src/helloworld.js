17cd0ddbdbc6ae688d3b69791eeb2e1cfb8c5219
Your Name <609116151@qq.com>
2020-05-23 12:08:27 +0800

    01

---
+++ 01/src/helloworld.js
@@ -0,0 +1,2 @@
+console.log('这是第一个例子，是否成功呢？');
+document.write('这是webpack初体验例子');
---
+++ 01/webpack.config.js
@@ -0,0 +1,10 @@
+'use strict'
+const path = require('path');
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'bundle.js'
+    }
+}
